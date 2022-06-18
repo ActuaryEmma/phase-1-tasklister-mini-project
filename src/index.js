7ba7f5d1a49a94ed148634ba2f50163e56908d1e
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-18 11:40:29 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const newTaskForm = document.getElementById("create-task-form");
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskUl = document.getElementById("tasks");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "x" 
+  newTask.appendChild(btn)
+
+  appendNewTask(newTask);
+  event.target.reset();
+  
+  console.log(newTask)
+  document.getElementById("task").appendChild(newTask)
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
+
+function handleDelete (e){
+    e.target.parentNode.remove}
